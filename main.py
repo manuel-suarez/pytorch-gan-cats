9092db1417f4124b39eb786b129c2423dfd337ee
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-06 21:08:02 -0600

    Generator code

--- main.py
+++ main.py
@@ -26,4 +26,32 @@ train_ds = ImageFolder(DATA_DIR, transform=tt.Compose([ tt.Resize(image_size),
 
 train_dl = DataLoader(train_ds, batch_size, shuffle=True, num_workers=3, pin_memory=True)
 
+generator = nn.Sequential(
+    # in: latent_size x 1 x 1
+
+    nn.ConvTranspose2d(latent_size, 512, kernel_size=4, stride=1, padding=0, bias=False),
+    nn.BatchNorm2d(512),
+    nn.ReLU(True),
+    # out: 512 x 4 x 4
+
+    nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(256),
+    nn.ReLU(True),
+    # out: 256 x 8 x 8
+
+    nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(128),
+    nn.ReLU(True),
+    # out: 128 x 16 x 16
+
+    nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(64),
+    nn.ReLU(True),
+    # out: 64 x 32 x 32
+
+    nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.Tanh()
+    # out: 3 x 64 x 64
+)
+
 print("Done!")
