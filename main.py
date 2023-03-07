6c74b9c2b3cc01cfb1cc57fb78ccf4ada99e042a
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-06 21:08:54 -0600

    Discriminator code

--- main.py
+++ main.py
@@ -54,4 +54,33 @@ generator = nn.Sequential(
     # out: 3 x 64 x 64
 )
 
+discriminator = nn.Sequential(
+    # in: 3 x 64 x 64
+
+    nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(64),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 64 x 32 x 32
+
+    nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(128),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 128 x 16 x 16
+
+    nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(256),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 256 x 8 x 8
+
+    nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(512),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 512 x 4 x 4
+
+    nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+    # out: 1 x 1 x 1
+
+    nn.Flatten(),
+    nn.Sigmoid())
+
 print("Done!")
