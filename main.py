b8d60dbdf94ed2915a8ae074e02bf2551bdff7de
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-06 21:03:08 -0600

    Load data

--- main.py
+++ main.py
@@ -13,5 +13,17 @@ import matplotlib.pyplot as plt
 DATA_DIR = '/home/est_posgrado_manuel.suarez/data/cats-faces/'
 
 print(os.listdir(DATA_DIR))
+print(os.listdir(DATA_DIR+'/cats')[:10])
+
+image_size = 64
+batch_size = 128
+stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+
+train_ds = ImageFolder(DATA_DIR, transform=tt.Compose([ tt.Resize(image_size),
+                                                        tt.CenterCrop(image_size),
+                                                        tt.ToTensor(),
+                                                        tt.Normalize(*stats)]))
+
+train_dl = DataLoader(train_ds, batch_size, shuffle=True, num_workers=3, pin_memory=True)
 
 print("Done!")
