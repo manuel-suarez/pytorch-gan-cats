d643d59a02abed1ebb45d713be601d2f7d47c391
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-06 21:00:29 -0600

    Fixes

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ import torchvision.transforms as tt
 import torch
 import torch.nn as nn
 import cv2
-from tqdm.notebook import tqdm
 import torch.nn.functional as F
 from torchvision.utils import save_image
 from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
-%matplotlib inline
 
 DATA_DIR = '/home/est_posgrado_manuel.suarez/data/cats-faces/'
 
