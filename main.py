357ea64bb6cb0d58cd9335e2a978b2d54b36e997
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-06 21:09:30 -0600

    Function to save example file

--- main.py
+++ main.py
@@ -83,4 +83,17 @@ discriminator = nn.Sequential(
     nn.Flatten(),
     nn.Sigmoid())
 
+sample_dir = 'generated'
+os.makedirs(sample_dir, exist_ok=True)
+
+def save_samples(index, latent_tensors, show=True):
+    fake_images = generator(latent_tensors)
+    fake_fname = 'generated-images-{0:0=4d}.png'.format(index)
+    save_image(denorm(fake_images), os.path.join(sample_dir, fake_fname), nrow=8)
+    print('Saving', fake_fname)
+    if show:
+        fig, ax = plt.subplots(figsize=(8, 8))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(fake_images.cpu().detach(), nrow=8).permute(1, 2, 0))
+
 print("Done!")
